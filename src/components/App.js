8d014e4b1adcef150ec923db8eba363c224586bc
Slobodan <slobodan.mitrovic1988@gmail.com>
2020-12-24 00:27:12 +0100

    added list of videos and video item

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import SearchBar from './SearchBar'
 import youtube from '../api/youtube'
+import VideoList from './VideoList'
 
 export class App extends Component {
     state = {
@@ -21,7 +22,7 @@ export class App extends Component {
         return (
             <div className="ui container">
                 <SearchBar onFormSubmit = {this.onTermSubmit}/>
-                I have {this.state.videos.length} videos
+                <VideoList videos={this.state.videos} />
             </div>
         )
     }
